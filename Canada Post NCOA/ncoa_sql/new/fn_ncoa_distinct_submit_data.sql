7bd8f50cec7548cab470bfcf7a38cfbb57aac4d1
haichun.zhang <1950720930.@qq.com>
2019-05-05 14:12:12 +0800

    ncoa review

--- Canada Post NCOA/ncoa_sql/new/fn_ncoa_distinct_submit_data.sql
+++ Canada Post NCOA/ncoa_sql/new/fn_ncoa_distinct_submit_data.sql
@@ -1,111 +1,116 @@
 DROP FUNCTION IF EXISTS fn_ncoa_distinct_submit_data(PARAM_BATCH_NO VARCHAR(32));
 CREATE OR REPLACE FUNCTION "fn_ncoa_distinct_submit_data"(PARAM_BATCH_NO VARCHAR(32))
-  RETURNS "pg_catalog"."void" AS $BODY$
+    RETURNS "pg_catalog"."void" AS $BODY$
+
 DECLARE
-  V_ADDRESS_DATA RECORD;
-  V_ADDRESS_DATA2 RECORD;
-  V_ADDRESS VARCHAR;
-  V_REFERENCE_MASTER_ID INT;
-  V_REFERENCE_MASTER_ID2 INT;
+    
+    /**
+     * Distinct the repeated address record.
+     */
+    V_ADDRESS_DATA RECORD;
+    V_ADDRESS_DATA2 RECORD;
+    V_ADDRESS VARCHAR;
+    V_REFERENCE_MASTER_ID INT;
+    V_REFERENCE_MASTER_ID2 INT;
+
 BEGIN
-  
-  FOR V_ADDRESS_DATA IN 
-    SELECT
-      COUNT (1),
-      nem."originalAddress" AS "address",
-      SUBSTRING (nem."originalPostalCode", 1, 3) AS "postalCode"
-    FROM ncoa_estate_master nem
-    WHERE 1 = 1
-      AND nem."batchNo" = PARAM_BATCH_NO
-
-    GROUP BY
-      nem."originalAddress",
-      SUBSTRING (nem."originalPostalCode", 1, 3)
-
-      HAVING
-        COUNT (1) = 2
-    ORDER BY
-      COUNT (1) DESC
-
-
-  LOOP
-
-    -- 查询地址信息相同但是 trade type = 1 的记录的 id.
-    V_REFERENCE_MASTER_ID := (
-        SELECT em."id" 
-        FROM estate_master em
+    
+    -- First Filteration. (address + postal code)
+    FOR V_ADDRESS_DATA IN 
+        SELECT
+            COUNT (1),
+            nem."originalAddress" AS "address",
+            SUBSTRING (nem."originalPostalCode", 1, 3) AS "postalCode"
+        FROM ncoa_estate_master nem
         WHERE 1 = 1
-          AND em."tradeTypeId" = 1
-          AND em."accuracyAddress" = V_ADDRESS_DATA."address"
-          AND em."postalCode" LIKE V_ADDRESS_DATA."postalCode"
-        LIMIT 1
-      );
-
-    UPDATE ncoa_estate_master
-    SET "referenceMasterId" = V_REFERENCE_MASTER_ID, "ncoaActiveFlag" = 'N'
-    FROM (
-            SELECT em."id"
-            FROM  estate_master em 
-            WHERE 1 = 1
-              AND em."tradeTypeId" = 2
-              AND em."accuracyAddress" = V_ADDRESS_DATA."address"
-              AND em."postalCode" LIKE V_ADDRESS_DATA."postalCode"
-          ) em
-      WHERE
-        em."id" = ncoa_estate_master."masterId";
-
-  END LOOP;
+            AND nem."batchNo" = PARAM_BATCH_NO
+        GROUP BY
+            nem."originalAddress",
+            SUBSTRING (nem."originalPostalCode", 1, 3)
+        HAVING
+            COUNT (1) = 2
+        ORDER BY
+            COUNT (1) DESC
 
 
-  <<LABEL1>> 
-  FOR V_ADDRESS_DATA2 IN 
-    SELECT
-      COUNT(1),
-      nem."originalAddress" AS "address",
-      nem."originalProvince" AS "province",
-      nem."originalCity" AS "city"
-    FROM ncoa_estate_master nem
-    WHERE 1 = 1
-      AND nem."batchNo" = PARAM_BATCH_NO
-    GROUP BY
-      nem."originalAddress",
-      nem."originalProvince",
-      nem."originalCity"
+    LOOP
 
-      HAVING
-        COUNT (1) = 2
-    ORDER BY
-      COUNT (1) DESC
-
-  LOOP
-
-    -- 查询地址信息相同但是 trade type = 1 的记录的 id.
-    V_REFERENCE_MASTER_ID2 := (
-        SELECT em."id"
-        FROM estate_master em 
-        WHERE 1 = 1
-          AND em."tradeTypeId" = 1
-          AND em."accuracyAddress" = V_ADDRESS_DATA2."address"
-          AND em."province" = V_ADDRESS_DATA2."province"
-          AND em."city" = V_ADDRESS_DATA2."city"
-        LIMIT 1
-      );
-
-    UPDATE ncoa_estate_master
-    SET "referenceMasterId" = V_REFERENCE_MASTER_ID2,"ncoaActiveFlag" = 'N'
-    FROM (
-            SELECT em."id"
+        -- Abstract the record that trade_type_id = 1
+        V_REFERENCE_MASTER_ID := (
+            SELECT em."id" 
             FROM estate_master em
             WHERE 1 = 1
-              AND em."tradeTypeId" = 2
-              AND em."accuracyAddress" = V_ADDRESS_DATA2."address"
-              AND em."province" = V_ADDRESS_DATA2."province"
-              AND em."city" = V_ADDRESS_DATA2."city"
-          ) em
-    WHERE
-      em."id" = ncoa_estate_master."masterId";
+                AND em."tradeTypeId" = 1
+                AND em."accuracyAddress" = V_ADDRESS_DATA."address"
+                AND em."postalCode" LIKE V_ADDRESS_DATA."postalCode"
+            LIMIT 1
+        );
 
-  END LOOP LABEL1; 
+        -- Update master_id of trade_type_id = 2
+        UPDATE ncoa_estate_master
+        SET "referenceMasterId" = V_REFERENCE_MASTER_ID, "ncoaActiveFlag" = 'N'
+        FROM (
+                SELECT em."id"
+                FROM  estate_master em 
+                WHERE 1 = 1
+                    AND em."tradeTypeId" = 2
+                    AND em."accuracyAddress" = V_ADDRESS_DATA."address"
+                    AND em."postalCode" LIKE V_ADDRESS_DATA."postalCode"
+            ) em
+        WHERE
+            em."id" = ncoa_estate_master."masterId";
+
+    END LOOP;
+
+
+    -- Second Filteration. (address + city + province)
+    <<LABEL1>> 
+    FOR V_ADDRESS_DATA2 IN 
+        SELECT
+            COUNT(1),
+            nem."originalAddress" AS "address",
+            nem."originalProvince" AS "province",
+            nem."originalCity" AS "city"
+        FROM ncoa_estate_master nem
+        WHERE 1 = 1
+            AND nem."batchNo" = PARAM_BATCH_NO
+        GROUP BY
+            nem."originalAddress",
+            nem."originalProvince",
+            nem."originalCity"
+        HAVING COUNT (1) = 2
+        ORDER BY COUNT (1) DESC
+
+    LOOP
+
+        -- Search id of trade_type_id = 1 record.
+        V_REFERENCE_MASTER_ID2 := (
+            SELECT em."id"
+            FROM estate_master em 
+            WHERE 1 = 1
+                AND em."tradeTypeId" = 1
+                AND em."accuracyAddress" = V_ADDRESS_DATA2."address"
+                AND em."province" = V_ADDRESS_DATA2."province"
+                AND em."city" = V_ADDRESS_DATA2."city"
+            LIMIT 1
+        );
+
+        -- Update the record that trade_type_id = 2
+        UPDATE ncoa_estate_master
+        SET "referenceMasterId" = V_REFERENCE_MASTER_ID2,"ncoaActiveFlag" = 'N'
+        FROM (
+                SELECT em."id"
+                FROM estate_master em
+                WHERE 1 = 1
+                    AND em."tradeTypeId" = 2
+                    AND em."accuracyAddress" = V_ADDRESS_DATA2."address"
+                    AND em."province" = V_ADDRESS_DATA2."province"
+                    AND em."city" = V_ADDRESS_DATA2."city"
+            ) em
+        WHERE
+            em."id" = ncoa_estate_master."masterId";
+
+    END LOOP LABEL1; 
       
 END;
 $BODY$
--- Canada Post NCOA/ncoa_sql/new/fn_ncoa_get_upload_data.sql
+++ Canada Post NCOA/ncoa_sql/new/fn_ncoa_get_upload_data.sql
@@ -4,137 +4,128 @@ CREATE OR REPLACE FUNCTION "fn_ncoa_get_upload_data"()
 
 DECLARE
   
-  /**
-   * 整理出一批需要 ncoa 的数据。
-   * 1. 从 estate_master 表中将符合条件的数据导出到 ncoa_estate_master 中，
-   * 记录批次号
-   * 2. 去除当前批次中地址重复的记录
-   * 3. 将去重之后的数据导入到 ncoa_submit 表中。
-   */
+    /**
+     * Put together a batch of data.
+     * Use the data to carry out NCOA operation.
+     */
 
-  V_BATCH_NO VARCHAR(32);
-  V_NCOA_COUNT_LIMIT INTEGER DEFAULT 3;
-  V_VOID VARCHAR(32);
+    V_BATCH_NO VARCHAR(32);
+    V_NCOA_COUNT_LIMIT INTEGER DEFAULT 3;
   
 BEGIN
   
-  -- TRUNCATE TABLE ncoa_estate_master RESTART IDENTITY CASCADE;
 
-  -- 生成 batch NO
-	V_BATCH_NO := TO_CHAR(CURRENT_TIMESTAMP, 'YYYYMMDDHH24MISS');
+    V_BATCH_NO := TO_CHAR(CURRENT_TIMESTAMP, 'YYYYMMDDHH24MISS');
   
-  -- 从 estate_master 表中查询数据, 插入到 ncoa_estate_master 表中
- 	INSERT INTO ncoa_estate_master(
-	 		"masterId", 
-      "batchNo",
-      "createdDate",
-	 		"originalFirstName", 
-	 		"originalLastName",
-	 		"originalAddress", 
-	 		"originalCity", 
-	 		"originalProvince", 
-	 		"originalPostalCode",
-      "preProjectClosingDate",
-      "createdTimestamp"
- 		)
- 	SELECT 
- 		em.id,
-    V_BATCH_NO,
-    CURRENT_DATE,
- 		em."preMoverFirstName",
- 		em."preMoverLastName",
- 		em."accuracyAddress",
- 		em."city",
- 		em."province",
- 		em."postalCode",
-    em."projectClosingDate",
-    CURRENT_TIMESTAMP
- 	FROM estate_master em
-    LEFT JOIN ncoa_estate_master n ON em."id" = n."masterId"
- 	WHERE em."recActiveFlag" = 'Y'
- 		AND em."activeFlag" = 'Y'
-    AND em."iAddressFlag" = 'Y'
- 		AND em."postMoverFirstName" IS NULL
- 		AND em."postMoverLastName" IS NULL
-    AND (em."preMoverFirstName" IS NOT NULL)
-    AND LENGTH(em."preMoverFirstName") > 2 -- First Name 必须是非简写的形式
-    AND em."preMoverFirstName" ~ '[a-zA-Z]'
-    AND em."preMoverLastName" IS NOT NULL
-    AND LENGTH(em."preMoverLastName") > 2 -- Last Name 也必须是非简写的形式
-    AND em."preMoverLastName" ~ '[a-zA-Z]'
-    AND n."masterId" IS NULL -- 保证 ncoa_estate_master 表中 'masterId' 的唯一性
-    AND em."delistedDate" <= (CURRENT_DATE - INTERVAL '90 DAYS')
-    AND em."delistedDate" >= (CURRENT_DATE - INTERVAL '120 DAYS')
- 	LIMIT 1;
+    -- 从 estate_master 表中查询数据, 插入到 ncoa_estate_master 表中
+    INSERT INTO ncoa_estate_master(
+        "masterId", 
+        "batchNo",
+        "createdDate",
+        "originalFirstName", 
+        "originalLastName",
+        "originalAddress", 
+        "originalCity", 
+        "originalProvince", 
+        "originalPostalCode",
+        "preProjectClosingDate",
+        "createdTimestamp"
+    )
+    SELECT 
+        em.id,
+        V_BATCH_NO,
+        CURRENT_DATE,
+        em."preMoverFirstName",
+        em."preMoverLastName",
+        em."accuracyAddress",
+        em."city",
+        em."province",
+        em."postalCode",
+        em."projectClosingDate",
+        CURRENT_TIMESTAMP
+    FROM estate_master em
+        LEFT JOIN ncoa_estate_master n ON em."id" = n."masterId"
+    WHERE 1 = 1
+        AND n."masterId" IS NULL -- Diff the duplicate record.
+        AND em."delistedDate" <= (CURRENT_DATE - INTERVAL '90 DAYS')
+        AND em."delistedDate" >= (CURRENT_DATE - INTERVAL '120 DAYS')
+        AND COALESCE(em."postMoverFirstName", '') = ''
+        AND COALESCE(em."postMoverLastName", '') = ''
+        AND COALESCE(em."preMoverFirstName", '') != ''
+        AND COALESCE(em."preMoverLastName", '') != ''
+        AND em."preMoverFirstName" SIMILAR TO '[a-zA-Z]{2,}'
+        AND em."preMoverLastName" SIMILAR TO '[a-zA-Z]{2,}'
+        AND em."recActiveFlag" = 'Y'
+        AND em."activeFlag" = 'Y'
+        AND em."iAddressFlag" = 'Y'
+    LIMIT 1;
 
-  -- 更新创建日期，以便进行多次 NCOA.
-  -- 按照指定的时间间隔。
-  -- UPDATE ncoa_estate_master
-  -- SET "createdDate" = CURRENT_DATE
-  -- WHERE "recActiveFlag" = 'Y'
-  --   AND "ncoaActiveFlag" = 'Y'
-  --   -- 下面是做 ncoa 的指定时间间隔
-  --   AND "createdDate" <= CURRENT_DATE - INTERVAL '30 DAYS' 
-  --   AND "createdDate" >= CURRENT_DATE - INTERVAL '36 DAYS';
+    -- Update created_date
+    -- According to specified space of time.
+    UPDATE ncoa_estate_master
+    SET "createdDate" = CURRENT_DATE
+    WHERE 1 = 1
+        AND "createdDate" <= CURRENT_DATE - INTERVAL '30 DAYS' 
+        AND "createdDate" >= CURRENT_DATE - INTERVAL '36 DAYS';
+        AND "ncoaActiveFlag" = 'Y'
+        AND "recActiveFlag" = 'Y'
 
-  -- 更新这批数据的 batch No 和 ncoa workflow status.
-  -- UPDATE ncoa_estate_master 
-  -- SET "ncoaWorkflowStatusId" = 1, "batchNo" = V_BATCH_NO  -- NCOA Init Data
-  -- WHERE "recActiveFlag" = 'Y'
-  --   AND "ncoaActiveFlag" = 'Y'
-  --   AND "createdDate" = CURRENT_DATE;
+    -- Update ncoa state.
+    UPDATE ncoa_estate_master 
+    SET "ncoaWorkflowStatusId" = 1, "batchNo" = V_BATCH_NO  -- NCOA Init Data
+    WHERE "recActiveFlag" = 'Y'
+        AND "ncoaActiveFlag" = 'Y'
+        AND "createdDate" = CURRENT_DATE;
 
-  /**
-   * 对当前批次数据进行去重。
-   */
-  -- PERFORM fn_ncoa_distinct_submit_data(V_BATCH_NO);
+    -- Distinct the repeated address records.
+    PERFORM fn_ncoa_distinct_submit_data(V_BATCH_NO);
 
-  -- 向 ncoa_submit 表中插入数据，此表中的数据是需要向
-  -- NCOA 提交的最终数据。
-  INSERT INTO ncoa_submit(
-  		"masterId",
-  		"batchNo",
-  		"preMoverFirstName",
-  		"preMoverLastName",
-  		"normalizedAddress",
-  		"city",
-  		"province",
-  		"postalCode",
-      "createdTimestamp"
-  	)
-  SELECT
-  	nem."masterId",
-  	nem."batchNo",
-  	nem."originalFirstName",
-  	nem."originalLastName",
-  	nem."originalAddress",
-  	nem."originalCity",
-  	nem."originalProvince",
-  	nem."originalPostalCode",
-    CURRENT_TIMESTAMP
-  FROM ncoa_estate_master nem
-  WHERE "recActiveFlag" = 'Y'
-  	AND "ncoaActiveFlag" = 'Y'
-    AND "batchNo" = V_BATCH_NO;
+    -- Insert data records into noca_submit table that
+    -- submitting the data to NCOA.
+    INSERT INTO ncoa_submit(
+        "masterId",
+        "batchNo",
+        "preMoverFirstName",
+        "preMoverLastName",
+        "normalizedAddress",
+        "city",
+        "province",
+        "postalCode",
+        "createdTimestamp"
+    )
+    SELECT
+        nem."masterId",
+        nem."batchNo",
+        nem."originalFirstName",
+        nem."originalLastName",
+        nem."originalAddress",
+        nem."originalCity",
+        nem."originalProvince",
+        nem."originalPostalCode",
+        CURRENT_TIMESTAMP
+    FROM ncoa_estate_master nem
+    WHERE "recActiveFlag" = 'Y'
+        AND "ncoaActiveFlag" = 'Y'
+        AND "batchNo" = V_BATCH_NO;
 
-  -- 数据提交到 ncoa 之后，
-  -- 更新 ncoa count 和 ncoa workflow status.
-  UPDATE ncoa_estate_master
-  SET "ncoaCount" = "ncoaCount" + 1, "ncoaWorkflowStatusId" = 2
-  WHERE "recActiveFlag" = 'Y'
-  	AND "ncoaActiveFlag" = 'Y'
-    AND "batchNo" = V_BATCH_NO;
+    -- After submitting the records, update the ncoa state.
+    UPDATE ncoa_estate_master
+    SET "ncoaCount" = "ncoaCount" + 1, "ncoaWorkflowStatusId" = 2
+    WHERE "recActiveFlag" = 'Y'
+    	AND "ncoaActiveFlag" = 'Y'
+        AND "batchNo" = V_BATCH_NO;
 
-  -- 通过 ncoa count 更新 ncoa active flag.
-  UPDATE ncoa_estate_master   
-  SET "ncoaActiveFlag" = 'N'
-  WHERE "recActiveFlag" = 'Y'
-  	AND "ncoaActiveFlag" = 'Y'
-  	AND "ncoaCount" >= V_NCOA_COUNT_LIMIT;
+    -- If a record have NCOA many times, Inactivate the record.
+    -- there is a bound.
+    UPDATE ncoa_estate_master   
+    SET "ncoaActiveFlag" = 'N'
+    WHERE "recActiveFlag" = 'Y'
+    	AND "ncoaActiveFlag" = 'Y'
+    	AND "ncoaCount" >= V_NCOA_COUNT_LIMIT;
 
-  RETURN V_BATCH_NO;
+    RETURN V_BATCH_NO;
 
 END 
 $BODY$
-  LANGUAGE 'plpgsql' VOLATILE
+    LANGUAGE 'plpgsql' VOLATILE
 ;
